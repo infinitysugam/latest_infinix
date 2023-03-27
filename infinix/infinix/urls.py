1a101fe27d0c32b83449cff433e27ad940c6ebf7
Sugam Mishra <mishrasugam11@gmail.com>
2023-03-27 05:47:33 +0545

    asda

--- infinix/infinix/urls.py
+++ infinix/infinix/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from todo_list import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todo/',include('todo_list.urls')),
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('login/',auth_views.LoginView.as_view(template_name='login.html'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='logout.html'),name='logout')
+
 ]
