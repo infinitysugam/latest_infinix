b371d7a25bd4d78cb8bf855ee824993ed9fcaf20
Sugam Mishra <mishrasugam11@gmail.com>
2023-03-28 19:47:01 +0545

    Second

--- infinix/infinix/settings.py
+++ infinix/infinix/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo_list.apps.TodoListConfig'
+    'todo_list.apps.TodoListConfig',
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,5 @@ STATICFILES_DIRS =[os.path.join(BASE_DIR,'static/')]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
--- infinix/infinix/urls.py
+++ infinix/infinix/urls.py
@@ -17,12 +17,15 @@ from django.contrib import admin
 from django.urls import path,include
 from todo_list import views
 from django.contrib.auth import views as auth_views
+from users import views as user_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todo/',include('todo_list.urls')),
     path('',views.home,name='home'),
+    path('register/',user_views.register,name='register'),
+    path('profile/',user_views.profile,name='profile'),
     path('login/',auth_views.LoginView.as_view(template_name='login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='logout.html'),name='logout')
 
