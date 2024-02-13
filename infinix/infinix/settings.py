64f853d004e37c86c13f24989463a158fb0b95e1
infinitysugam <mishrasugam11@gmail.com>
2024-02-12 22:27:21 -0600

    Setting Up

--- infinix/infinix/settings.py
+++ infinix/infinix/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'infinix',
         'USER':'root',
-        'PASSWORD':'infinity',
+        'PASSWORD':'root',
         'HOST':'127.0.0.1',
         'PORT':'3306'
     }
